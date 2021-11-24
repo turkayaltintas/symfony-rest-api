3c0c4c868775687c4dfc7fb3af5d1b17c1282853
Türkay ALTINTAŞ <muhammet.altintas@signumtte.com>
2021-11-24 21:03:18 +0300

    Part1

---
+++ src/Entity/Superheroes.php
@@ -0,0 +1,104 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Table(name="superheroes")
+ * @ApiResource(
+ *   normalizationContext={"groups" = {"read"}},
+ *   denormalizationContext={"groups" = {"write"}},
+ *   itemOperations={
+ *     "get",
+ *     "patch",
+ *     "delete",
+ *     "put",
+ *     "get_by_slug" = {
+ *       "method" = "GET",
+ *       "path" = "/superhero/{slug}",
+ *       "controller" = SuperheroBySlug::class,
+ *       "read"=false,
+ *       "openapi_context" = {
+ *         "parameters" = {
+ *           {
+ *             "name" = "slug",
+ *             "in" = "path",
+ *             "description" = "The slug of your superhero",
+ *             "type" = "string",
+ *             "required" = true,
+ *             "example"= "superman",
+ *           },
+ *         },
+ *       },
+ *     },
+ *   },
+ * )
+ */
+class Superheroes
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @Groups({"read"})
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(length=70)
+     * @Assert\NotBlank()
+     * @Groups({"read", "write"})
+     */
+    public string $name;
+
+    /**
+     * @ORM\Column(length=70, unique=true)
+     * @Assert\NotBlank()
+     * @Groups({"read", "write"})
+     */
+    public string $slug;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Groups({"read", "write"})
+     */
+    public bool $featured = false;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"read"})
+     */
+    public ?\DateTime $created_at = null;
+
+
+
+    /******** METHODS ********/
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Prepersist gets triggered on Insert
+     * @ORM\PrePersist
+     */
+    public function updatedTimestamps()
+    {
+        if ($this->created_at == null) {
+            $this->created_at = new \DateTime('now');
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+}
